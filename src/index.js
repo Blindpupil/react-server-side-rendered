5564dc9252418424a949c73a6f96d48e7d243ba4
Cesar Martinez <cesar.martinez@globalgaming.com>
2018-08-14 16:24:20 +0200

    Routes refactor with config, and matchRoutes function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill'
 import express from 'express'
+import { matchRoutes } from 'react-router-config'
+import Routes from './client/Routes'
 import renderer from './helpers/renderer'
 import createStore from './helpers/createStore'
 
@@ -10,8 +12,9 @@ app.use(express.static('public'))
 
 app.get('*', (req, res) => {
   const store = createStore()
-  /* Here goes the logic to
-  load data into the store */
+
+  matchRoutes(Routes, req.path)
+
   res.send(renderer(req, store))
 })
 
