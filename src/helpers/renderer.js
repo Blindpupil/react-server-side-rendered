2a0a94570f9ba937aa6a9b5892ea66d007a6e6d5
Cesar Martinez <cesar.martinez@globalgaming.com>
2018-08-15 10:53:49 +0200

    Set client-side initial state to what server produces

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import Routes from '../client/Routes'
 import { renderRoutes } from 'react-router-config'
+import serialize from 'serialize-javascript'
+import Routes from '../client/Routes'
 
 export default (req, store) => {
   const content = renderToString(
@@ -19,6 +20,7 @@ export default (req, store) => {
       <head>
         <body>
           <div id="app">${content}</div>
+          <script>window.INITIAL_STATE = ${serialize(store.getState())}</script>
           <script src="bundle.js"></script>
         </body>
       </head>
